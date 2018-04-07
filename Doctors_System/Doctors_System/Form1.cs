c63fe2a346e7308916b43f76a241956baf552759
Chayanne <chayannesuazo@gmail.com>
2018-04-07 18:37:01 -0400

    Avance de menu principal : Trancicion de menu despegable, colores y Botones de funcionales.

--- Doctors_System/Doctors_System/Form1.cs
+++ Doctors_System/Doctors_System/Form1.cs
@@ -16,5 +16,53 @@ namespace Doctors_System
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void bunifuImageButton2_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuFlatButton5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Logo_Click(object sender, EventArgs e)
+        {
+            if (panel1.Width==223)
+            {
+                panel1.Visible = false;
+                //panel hidde
+                panel1.Width = 60;
+                Logo.Visible = false;
+                panelanimation.ShowSync(panel1);
+                logomenu.ShowSync(Logo);
+                
+            }
+            else 
+                    {
+                
+                panel1.Visible = false;
+                panel1.Width = 223;
+                Logo.Visible = true;
+                panelanimation.ShowSync(panel1);
+
+            }
+
+        }
     }
 }
